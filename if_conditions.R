98be48a89cfd1d9f3f31f968f93cd59f9033c7b3
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-07-15 14:20:58 -0400

    if conditions update - complete

--- if_conditions.R
+++ if_conditions.R
@@ -1,4 +1,7 @@
-# If conditions
+# If conditions: 
+#if, 
+#if/else, 
+#if/ else if/else
 
 a <- 7
 
@@ -8,6 +11,23 @@ if (a>3){
 
 a<- 2
 if (a>3){
-  print("a is bigger than a")
-else 
+  print("a is bigger than 3")
+}else {
+  print("a is NOT bigger than 3")
 }
+
+a <- 5
+if (a>3){
+  print("a is greater than 3")
+} else if (a==3) {
+  print("a is equal to 3")
+} else {
+  print("a is smaller than 3")
+}
+
+# Attention: after else, no need to specify condition, since it covers all the rest
+#```r
+
+#ifselse function
+a <- 5
+ifelse(a>3,"a is bigger than 3","a is not bigger than 3")
