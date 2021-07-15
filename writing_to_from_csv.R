22c10690e345fcd48e012f392f435db6234b16af
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-07-15 10:08:06 -0400

    how to read and write different formats

--- writing_to_from_csv.R
+++ writing_to_from_csv.R
@@ -13,3 +13,37 @@ data3 <- airquality
 write.xlsx(data3, "data3.xls")
 
 data4 <- read.xlsx("data3.xlsx")
+
+
+## READING AND WRITING TABLES (Matrices and Data Frame Section)
+# from link (online data) or from working directory
+
+read.table() # text format
+read.csv() # csv
+read.xlsx() # excel , install xlsx packages
+readLines() # lines of text file
+source() # R code
+
+write.table()
+write.csv()
+write.xlsx()
+writeLines()
+
+#Examples:
+
+#Online sale data
+# Read online csv data:
+salesdata <-read.csv('https://support.spatialkey.com/wp-content/uploads/2021/02/Sacramentorealestatetransactions.csv')
+dim(salesdata) #nrows, ncol
+names(salesdata) # column names
+
+#Save output to csv file
+write.csv(salesdata, 'SalesData.csv')
+
+# Example 2:
+#Reading POEM
+poem <- readLines("http://lib.ru/SHAKESPEARE/sonnets.txt")
+poem[10:20]
+
+#Writing poem :
+writeLines(poem[10:20], "Sonet1.txt")
