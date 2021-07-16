3da0c499533892eb6316380e0ef3cd3a582e11fc
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2021-07-16 10:39:27 -0400

    added short for loop syntax and explanation

--- for_loops.R
+++ for_loops.R
@@ -1,16 +1,29 @@
 #For loops:
-
 for (i in 1:5){
   print(i)
 }
 
+for (i in sequence(1, 5)){
+  print(i)
+}
+
+for (iterator in sequence){ #full syntax
+  do task
+}
+
+for (i in 1:5) print(i) # short syntax
+
+for (i in seq(1, 5)) print(letters[i]) 
 
 # While loops
 while (a<5){
   print(a)
   a <- a+1
 }
-# 
+
+while (this condition is true){ # syntax
+  do a thing
+}
 
 
 # For loops are on a sequence
